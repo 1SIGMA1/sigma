7394f04d337cdd9a07f11d66487cf9687071361e
1SIGMA1 <aleksandr.mezennoi@tptlive.ee>
2022-03-25 08:24:49 +0200

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -8,14 +8,37 @@ map1 = [
 map2 = [
     [12, 1, 0, 0],
     [0, 1, 0, 0],
-    [1, 0, 0, 0],
-    [1, 0, 0, 24]
+    [1, 0, 1, 0],
+    [1, 0, 1, 24]
 ]
 map=map1
 
 current_position_x = 0
 current_position_y = 0
 
+def move(step, current_position_x, current_position_y):
+    print(f"le step - {step}")
+    print(f"le position x - {current_position_x}")
+    print(f"le position y - {current_position_y}")
+    can_move_right = map[current_position_y][current_position_x+1] == 0
+    can_move_left = map[current_position_y+1][current_position_x] == 0
+
+    if can_move_right:
+        print("should move le right")
+        current_position_x = current_position_x+1
+    if can_move_left:
+        print("le should move le down")
+        current_position_y = current_position_y+1
+    
+    return[current_position_x, current_position_y]
+
+new_position = move(1, current_position_x, current_position_y)
+new_position = move(2, new_position[0], new_position[1])
+new_position = move(3, new_position[0], new_position[1])
+new_position = move(4, new_position[0], new_position[1])
+new_position = move(5, new_position[0], new_position[1])
+
+'''
 # le step 1
 print(f"le position x - {current_position_x}")
 print(f"le position y - {current_position_y}")
@@ -29,60 +52,7 @@ if can_move_left:
     print("le should move le down")
     current_position_y = current_position_y+1
 
-# le step 2
-print(f"le position x - {current_position_x}")
-print(f"le position y - {current_position_y}")
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_left = map[current_position_y+1][current_position_x] == 0
 
-if can_move_right:
-    print("should move le right")
-    current_position_x = current_position_x+1
-if can_move_left:
-    print("le should move le down")
-    current_position_y = current_position_y+1
-
-# le step 3
-print(f"le position x - {current_position_x}")
-print(f"le position y - {current_position_y}")
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_left = map[current_position_y+1][current_position_x] == 0
-
-if can_move_right:
-    print("should move le right")
-    current_position_x = current_position_x+1
-if can_move_left:
-    print("le should move le down")
-    current_position_y = current_position_y+1
-
-# le step 4
-print(f"le position x - {current_position_x}")
-print(f"le position y - {current_position_y}")
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_left = map[current_position_y+1][current_position_x] == 0
-
-if can_move_right:
-    print("should move le right")
-    current_position_x = current_position_x+1
-if can_move_left:
-    print("le should move le down")
-    current_position_y = current_position_y+1
-
-# le step 5
-print(f"le position x - {current_position_x}")
-print(f"le position y - {current_position_y}")
-can_move_right = map[current_position_y][current_position_x+1] == 0
-can_move_left = map[current_position_y+1][current_position_x] == 0
-
-if can_move_right:
-    print("should move le right")
-    current_position_x = current_position_x+1
-if can_move_left:
-    print("le should move le down")
-    current_position_y = current_position_y+1
-
-
-'''
 print("can you move right")
 print(map[0][1] == 0)
 print("can you move to botom")
